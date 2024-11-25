f6a2ffc0ad52b79e091fd109fa4a9bed58b8b187
fmichalski03 <filip.michalski.1@student.put.poznan.pl>
2024-11-25 15:20:46 +0100

    add_new_model

---
+++ src_cm4sl/srcipt.m
@@ -0,0 +1,21 @@
+
+g=9.81;
+L=2.6;
+lr=0.765;
+lf=L-lr;
+h=0.25;
+m=180;
+ls=1.242;
+rw=0.217;
+drive_ratio=14.25;
+max_moment = 14;
+Ku=1/700*-1;
+Mz_p=700;
+Mz_I=70;
+batt_voltage=587;
+P_max=1;
+batt_curr=1;
+batt_max_curr=1;
+load("AeroFD.mat")
+load("AeroFL.mat")
+load("AeroFS.mat")
